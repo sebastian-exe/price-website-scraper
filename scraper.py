f06e8bd2adf472caf47540fdb035050c59be54be
Sebastian Moreno <46652667+sebastian-exe@users.noreply.github.com>
2019-07-10 15:10:28 -0500

    the most current version
    finally was able to parse through the html correctly. just need to set up the server for the email when the price drops to notify the user. Also have to go through and set up the two step authentication so that the server can send an email to the client.

--- scraper.py
+++ scraper.py
@@ -1,19 +1,32 @@
 import requests
+import smtplib
 from bs4 import BeautifulSoup
 
-URL = 'https://www.newegg.com/asus-90lm01h1-b013b0-21-5-full-hd/p/N82E16824236974'
+URL = 'https://www.amazon.de/Sony-Digitalkamera-Touch-Display-Vollformatsensor-Kartenslots/dp/B07NLTXWZT/ref=sr_1_3?th=1'
 
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15'}
 
-page = requests.get(URL, headers=headers)
+def check_price():
+    page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-title = soup.find(id = "grpDescrip_24-236-974").get_text()
-price = soup.find(id = "strong")
-#converted_price = price[0:5]
+    title = soup.find(id = "productTitle").get_text()
+    price = soup.find(id = "priceblock_ourprice").get_text()
+    converted_price = float(price[3:9])
 
-print(title)
-print(price)
+    #if(converted_price < 3.910)
+    send_mail()
+
+    print(title.strip())
+    print(converted_price)
+
+def send_mail():
+    #add the server number thingy 
+    server = smtplib.SMTP('smtp.gmail.com',)
+    server.ehlo()
+    server.starttls
+    server.elho()
+    server.login()
 
